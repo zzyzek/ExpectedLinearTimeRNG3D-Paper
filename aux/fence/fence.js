c4140d0fa86254218aa860c1dabb8e382d563e86
zzyzek <zzyzek@thumpernet.com>
2026-08-31 11:23:12 -0400

    data for fence fig

---
+++ aux/fence/fence.js
@@ -0,0 +1,49 @@
+
+let S = 0.5;
+var fence = [
+  [ [ -1, -1, -1 ], [  1, -1, -1 ] ],
+  [ [ -1, -1, -1 ], [ -1,  1, -1 ] ],
+  [ [ -1, -1, -1 ], [ -1, -1,  1 ] ],
+
+  [ [  1,  1,  1 ], [ -1,  1,  1 ] ],
+  [ [  1,  1,  1 ], [  1, -1,  1 ] ],
+  [ [  1,  1,  1 ], [  1,  1, -1 ] ]
+];
+
+for (let i=0; i<fence.length; i++) {
+  for (let j=0; j<fence[i].length; j++) {
+    for (let k=0; k<3; k++) {
+      fence[i][j][k] *= S;
+    }
+  }
+}
+
+
+let p0 = [ 0.25, 0.5, -0.5 ];
+let p1 = [ -0.5, 0.5, 0.25 ];
+let p2 = [ 0.35, -0.5, -0.5 ];
+
+
+for (let i=0; i<fence.length; i++) {
+  let a = fence[i][0];
+  let b = fence[i][1];
+  console.log(a[0], a[1], a[2]);
+  console.log(b[0], b[1], b[2]);
+  console.log("\n\n");
+}
+
+console.log(p0[0], p0[1], p0[2]);
+console.log(p1[0], p1[1], p1[2]);
+console.log("\n\n");
+
+console.log(p1[0], p1[1], p1[2]);
+console.log(p2[0], p2[1], p2[2]);
+console.log("\n\n");
+
+
+console.log(p0[0], p0[1], p0[2]);
+console.log(p2[0], p2[1], p2[2]);
+console.log("\n\n");
+
+
+
